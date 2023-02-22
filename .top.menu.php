91c6ba83a6675005d0323f8aedd2e002071e3711
Redrik Dark <illarionow.artem@yandex.ru>
2023-02-22 12:12:32 +0300

    Выполнена подзадача [ex1-1]

--- .top.menu.php
+++ .top.menu.php
@@ -27,6 +27,13 @@ $aMenuLinks = Array(
 		Array(), 
 		Array(), 
 		"" 
+	),
+	Array(
+		"Партнерам", 
+		"/partners/", 
+		Array(), 
+		Array(), 
+		"" 
 	)
 );
 ?>
---
+++ partners/raspisanie-meropriyatiy/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Анонсы", 
+		"/partners/raspisanie-meropriyatiy/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Архив", 
+		"/partners/raspisanie-meropriyatiy/archive.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
