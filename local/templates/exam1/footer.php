730c77b459e26a6b800a7860391430da67e8760d
Redrik Dark <illarionow.artem@yandex.ru>
2023-02-27 12:25:40 +0300

    Выполнено [ex1-4] Создать шаблон сайта

---
+++ local/templates/exam1/footer.php
@@ -0,0 +1,223 @@
+<?
+use Bitrix\Main\Localization\Loc;
+use Bitrix\Main\Application;
+use Bitrix\Main\Web\Uri;
+
+Loc::loadMessages(__FILE__);
+
+$request = Application::getInstance()->getContext()->getRequest();
+$uri = new Uri($request->getRequestUri());
+?>
+                <!-- content -->
+                <?if ($uri->getUri() !== "/") :?>
+                <div class="content">
+                    <div class="cnt">
+                        <?if ($uri->getUri() !== "/") :?>
+                        <header>
+                            <h1>Заголовок страницы</h1>
+                        </header>
+                        <?endif?>
+                        <hr>
+                        <p>Если вы используете базовую редакцию продукта - «Компания», то можете перейти на редакцию «Совместная работа», и ваш портал приобретет все возможности для осуществления горизонтальных коммуникаций и управлению Экстранетом.
+                            Вы сможете организовать внутри компании коллективную работу над проектами в рабочих группах, вести учет и планирование времени и событий, обмениваться сообщениями и почтой через портал, проводить внутри компании видеоконференции
+                            и делать многое другое.</p>
+                        <p>Можете сразу перейти с «младшей», базовой редакции на «Бизнес-процессы». После такого перехода ваш портал можно будет интегрировать с внешним сайтом, на портале добавятся возможности визуального проектирования бизнес-процессов
+                            и управления списками, включая списки в рабочих группах. Кроме того, вы сможете анализировать посещаемость своего портала</p>
+                        <p>При переходе вся информация на портале сохранится. Вам не потребуется создавать сайт заново. С помощью технологии SiteUpdate вы получите новые модули продукта и установите их без помощи разработчиков.</p>
+                        <h2>Выставка новых образцов</h2>
+                        <hr>
+                        <div class="article-text-block">
+                            <figure class="pic-block">
+                                <img src="<?=SITE_TEMPLATE_PATH?>/img/pic01.jpg" alt="">
+                            </figure>
+                            <p>Ваш портал приобретет все возможности для осуществления горизонтальных коммуникаций и управлению Экстранетом. Вы сможете организовать внутри компании коллективную работу над проектами в рабочих группах, вести учет и планирование
+                                времени и событий, обмениваться сообщениями и почтой через портал, проводить внутри компании видеоконференции и делать многое другое.</p>
+                            <h3>Заголовок</h3>
+                            <p><i>Можете сразу перейти с «младшей», базовой редакции на «Бизнес-процессы». После такого перехода ваш портал можно будет интегрировать с внешним сайтом, на портале добавятся возможности визуального проектирования бизнес-процессов и управления списками, включая списки в рабочих группах. Кроме того, вы сможете анализировать посещаемость своего портала</i>
+                            </p>
+                            <p><span class="att-text">При переходе вся информация на портале сохранится.</span> 
+                                <br>Вам не потребуется создавать сайт заново. С помощью технологии SiteUpdate вы получите новые модули продукта и установите их без помощи разработчиков.</p>
+                        </div>
+                    </div>
+                </div>
+                <?endif?>
+                <!-- /content -->
+
+                <!-- side -->
+                <div class="side">
+                    <!-- side menu -->
+                    <div class="side-block side-menu">
+                        <div class="title-block">Навигация</div>
+                        <div class="menu-block">
+                            <ul>
+                                <li><a href="">Кухни</a>
+                                </li>
+                                <li><a href="">Гарнитуры</a>
+                                </li>
+                                <li class="selected">
+                                    <a href="">Спальни</a>
+                                </li>
+                                <li>
+                                    <a href="">Шкафы</a>
+                                </li>
+                                <li><a href="">Столы</a>
+                                </li>
+                                <li><a href="">Стулья</a>
+                                </li>
+                                <li>
+                                    <a href="">Прихожие</a>
+                                </li>
+                                <li><a href="">Диваны</a>
+                                </li>
+                                <li><a href="">Кресла</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /side menu -->
+                    <!-- side anonse -->
+                    <div class="side-block side-anonse">
+                        <div class="title-block"><span class="i i-title01"></span>Полезная информация!</div>
+                        <div class="item">
+                            <p>Клиенты предпочитают все больше эко-материалов.</p>
+                        </div>
+                    </div>
+                    <!-- /side anonse -->
+                    <!-- side wrap -->
+                    <div class="side-wrap">
+                        <div class="item-wrap">
+                            <!-- side action -->
+                            <div class="side-block side-action">
+                                <img src="<?=SITE_TEMPLATE_PATH?>/img/side-action-bg.jpg" alt="" class="bg">
+                                <div class="photo-block">
+                                    <img src="<?=SITE_TEMPLATE_PATH?>/img/side-action.jpg" alt="">
+                                </div>
+                                <div class="text-block">
+                                    <div class="title">Акция!</div>
+                                    <p>Мебельная полка всего за 560 <span class="r">a</span>
+                                    </p>
+                                </div>
+                                <a href="" class="btn-more">подробнее</a>
+                            </div>
+                            <!-- /side action -->
+                        </div>
+                        
+                       <!-- footer rew slider box -->
+                       <div class="item-wrap">
+		                     <div class="rew-footer-carousel">
+								<div class="item">
+									<div class="side-block side-opin">
+										<div class="inner-block">
+											<div class="title">
+												<div class="photo-block">
+													<img src="<?=SITE_TEMPLATE_PATH?>/img/side-opin.jpg" alt="">
+												</div>
+												<div class="name-block"><a href="">Дмитрий Иванов</a></div>
+												<div class="pos-block">Генеральный директор,"Офис+"</div>
+											</div>
+											<div class="text-block">“В магзине предоставили потрясающий выбор
+												расцветок, а также, получил большую скидку по карте постоянного...</div>
+										</div>
+									</div>
+								</div>
+								<div class="item">
+									<div class="side-block side-opin">
+										<div class="inner-block">
+											<div class="title">
+												<div class="photo-block">
+													<img src="<?=SITE_TEMPLATE_PATH?>/img/side-opin.jpg" alt="">
+												</div>
+												<div class="name-block"><a href="">Дмитрий Иванов</a></div>
+												<div class="pos-block">Генеральный директор,"Офис+"</div>
+											</div>
+											<div class="text-block">“В магазине предоставили потрясающий выбор
+												расцветок, а также, получил большую скидку по карте постоянного...</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- / footer rew slider box --> 
+                        
+                    </div>
+                    <!-- /side wrap -->
+                </div>
+                <!-- /side -->
+            </div>
+            <!-- /content box -->
+        </div>
+        <!-- /page -->
+        <div class="empty"></div>
+    </div>
+    <!-- /wrap -->
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="inner-wrap">
+            <nav class="main-menu">
+                <div class="item">
+                    <div class="title-block">О магазине</div>
+                    <ul>
+                        <li><a href="">Отзывы</a>
+                        </li>
+                        <li><a href="">Руководство </a>
+                        </li>
+                        <li><a href="">История</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="item">
+                    <div class="title-block">Каталог товаров</div>
+                    <ul>
+                        <li><a href="">Кухни</a>
+                        </li>
+                        <li><a href="">Гарнитуры</a>
+                        </li>
+                        <li><a href="">Спальни и матрасы</a>
+                        </li>
+                        <li><a href="">Столы и стулья</a>
+                        </li>
+                        <li><a href="">Раскладные диваны</a>
+                        </li>
+                        <li><a href="">Кресла</a>
+                        </li>
+                        <li><a href="">Кровати и кушетки</a>
+                        </li>
+                        <li><a href="">Тумобчки и прихожие</a>
+                        </li>
+                        <li><a href="">Аксессуары</a>
+                        </li>
+                        <li><a href="">Каталоги мебели</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="contacts-block">
+                <div class="title-block"><?=Loc::getMessage("CONTACTS_INFO");?></div>
+                <div class="loc-block">
+                    <div class="address">ул. Летняя, стр.12, офис 512</div>
+                    <div class="phone"><a href="tel:84952128506">8 (495) 212-85-06</a>
+                    </div>
+                </div>
+                <div class="main-soc-block">
+                    <a href="" class="soc-item">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/soc01.png" alt="">
+                    </a>
+                    <a href="" class="soc-item">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/soc02.png" alt="">
+                    </a>
+                    <a href="" class="soc-item">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/soc03.png" alt="">
+                    </a>
+                    <a href="" class="soc-item">
+                        <img src="<?=SITE_TEMPLATE_PATH?>/img/icons/soc04.png" alt="">
+                    </a>
+                </div>
+                <div class="copy-block">© 2000 - 2012 "Мебельный магазин"</div>
+            </div>
+        </div>
+    </footer>
+    <!-- /footer -->
+</body>
+
+</html>
