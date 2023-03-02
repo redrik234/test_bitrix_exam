1ac224d8746bec7e8056ab7e469b254740818f7d
Redrik Dark <illarionow.artem@yandex.ru>
2023-03-02 15:18:35 +0300

    [ex1-1] Добавить раздел сайта: Партнерам

---
+++ partners/raspisanie-meropriyatiy/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Анонсы", 
+		"/partners/raspisanie-meropriyatiy/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Архив", 
+		"/partners/raspisanie-meropriyatiy/arkhiv.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
